ac8171ff15791df24bef68bf195e99f1ea6ac7e5
larsenv <larsen@weborican.com>
2017-08-17 17:17:51 -0500

    Updated parsing

--- rss_displayer.php
+++ rss_displayer.php
@@ -15,34 +15,23 @@ echo "--".$wc24mimebounary."\r\n";
 echo "Content-Type: text/plain\r\n\r\n";
 echo "This part is ignored.\r\n\r\n\r\n";
 
-echo "--".$wc24mimebounary . "\r\n".
-"Content-Type: text/plain\r\n\r\n".
-"Date: " . gmdate('D, d M Y H:i:s') . " +0000 (UTC)\r\n".
-"From: w9999999900000000@rc24.xyz\r\n".
-"To: allusers@rc24.xyz\r\n".
-"Subject: " . $_REQUEST["title"] . "\r\n".
-"MIME-Version: 1.0\r\n".
-"Content-Type: text/plain; charset=ISO-8859-1\r\n".
-"Content-Transfer-Encoding: 7bit\r\n".
-"X-Wii-AltName: AFIAUwBTAE0AaQBpACAAVQBwAGQAYQB0AGU=\r\n".
-"X-Wii-MB-NoReply: 1\r\n\r\n";
-
-$feed_num = 0;
-$link_num = 0;
-
 foreach($feed->get_items() as $item)
 {
-	$feed_num++;
+	echo "--".$wc24mimebounary."\r\n".
+	"Content-Type: text/plain\r\n\r\n".
+	"Date: " . gmdate('D, d M Y H:i:s') . " +0000 (UTC)\r\n".
+	"From: w9999999900000000@rc24.xyz\r\n".
+	"To: allusers@rc24.xyz\r\n".
+	"Subject: \r\n".
+	"MIME-Version: 1.0\r\n".
+	"Content-Type: text/plain; charset=ISO-8859-1\r\n".
+	"Content-Transfer-Encoding: 7bit\r\n".
+	"X-Wii-AltName: " . base64_encode(mb_convert_encoding($_REQUEST["title"], "UTF-16", "auto")) . "\r\n".
+	"X-Wii-MB-NoReply: 1\r\n\r\n";
+
 	echo $item->get_title() . "\r\n\r\n".
-	$item->get_description() . " ".
-	"[" . (string) $feed_num . "]" . "\r\n\r\n";
-}
-
-foreach($feed->get_items() as $item)
-{
-	$link_num++;
-	echo "[" . (string) $link_num . "]".
-	" " . $item->get_link() . "\r\n";
+	$item->get_description() . '[1]' . "\r\n\r\n".
+	'[1]' . " " . $item->get_link() . "\r\n";
 }
 
 echo "--".$wc24mimebounary."--"."\r\n";
