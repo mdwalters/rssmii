0746429b93a7f8d5b1963cb9e840d5939095ece6
Larsenv <larsen@weborican.com>
2017-10-20 06:48:11 -0500

    Check back in place, fuck you

--- rss_displayer.php
+++ rss_displayer.php
@@ -13,12 +13,14 @@ $wc24mimebounary = "BoundaryForDL" . date("YmdHi") . "/" . rand(1000000, 9999999
 
 echo "--".$wc24mimebounary."\r\n";
 echo "Content-Type: text/plain\r\n\r\n";
-echo "This part is ignored.\r\n\r\n\r\n";	
+echo "This part is ignored.\r\n\r\n\r\n";
 
 foreach($feed->get_items() as $item)
 {
+	if (time() - strtotime($item->get_date(), $now = time()) <= 3600)
+	{
 		/* Create the main body text. */
-	
+
 		echo "--".$wc24mimebounary."\r\n".
 		"Content-Type: text/plain\r\n\r\n".
 		"Date: " . gmdate('D, d M Y H:i:s') . " +0000 (UTC)\r\n".
@@ -30,19 +32,19 @@ foreach($feed->get_items() as $item)
 		"Content-Transfer-Encoding: 7bit\r\n".
 		"X-Wii-AltName: " . base64_encode(mb_convert_encoding($_REQUEST["title"], "UTF-16", "auto")) . "\r\n".
 		"X-Wii-MB-NoReply: 1\r\n\r\n";
-	
+
 		$raw_description = new \Html2Text\Html2Text($item->get_description());
 		$description = $raw_description->getText();
 
 		echo $description . "\r\n\r\n" . "Link: " . $item->get_link() . "\r\n\r\n";
-	
+
 		/* Create the chjump used for opening the link in the (ew) Internet Channel. */
-		
+
 		/*echo "--".$wc24mimebounary."\r\n".
 		"Content-Type: Application/X-Wii-MsgBoard; name=internet_jump.arc\r\n\r\n".
 		"Content-Transfer-Encoding: base64\r\n".
 		"Content-Disposition: attachment; filename=internet_jump.arc\r\n\r\n";
-		
+
 		$h = pack("H*", "43684A7000000034000000010000000000010001484144410000002000000014") . $item->get_link();
 		mkdir("./tmp");
 		$f = fopen("./tmp/chjump.bin", "wb+");
@@ -52,6 +54,7 @@ foreach($feed->get_items() as $item)
 		$chjump = base64_encode(fread($u8_output, filesize("tmp.u8")));
 		echo $chjump . "\r\n\r\n";
 		fclose($u8_output);*/
+	}
 }
 echo "--".$wc24mimebounary."--"."\r\n\r\n";
 ?>
