a3497c53b0854f1a0958c944d3f14dddf60fce5a
Andreas Bielawski <brawl98@outlook.com>
2019-05-10 20:54:47 +0200

    Tabs -> Spaces

--- rssmii_displayer/rss_displayer.php
+++ rssmii_displayer/rss_displayer.php
@@ -26,43 +26,42 @@ foreach($feed->get_items() as $item) {
         continue;
     }
 
-	/* Create the main body text. */
+    /* Create the main body text. */
 
-	echo "--".$wc24mimebounary."\r\n".
-	"Content-Type: text/plain\r\n\r\n".
-	"Date: " . gmdate('D, d M Y H:i:s') . " +0000 (UTC)\r\n".
-    //"Date: " . $item->get_gmdate("D, d M Y H:i:s") . " +0000 (UTC)\r\n".
-	"From: w9999999900000000@rc24.xyz\r\n".
-	"To: allusers@rc24.xyz\r\n".
-	"Subject: \r\n".
-	"MIME-Version: 1.0\r\n".
-	"Content-Type: text/plain; charset=utf-8\r\n".
-	"Content-Transfer-Encoding: 7bit\r\n".
-	"X-Wii-AltName: " . base64_encode(mb_convert_encoding($_REQUEST["title"], "UTF-16", "auto")) . "\r\n".
-	"X-Wii-MB-NoReply: 1\r\n\r\n";
+    echo "--".$wc24mimebounary."\r\n".
+    "Content-Type: text/plain\r\n\r\n".
+    "Date: " . gmdate('D, d M Y H:i:s') . " +0000 (UTC)\r\n".
+    "From: w9999999900000000@rc24.xyz\r\n".
+    "To: allusers@rc24.xyz\r\n".
+    "Subject: \r\n".
+    "MIME-Version: 1.0\r\n".
+    "Content-Type: text/plain; charset=utf-8\r\n".
+    "Content-Transfer-Encoding: 7bit\r\n".
+    "X-Wii-AltName: " . base64_encode(mb_convert_encoding($_REQUEST["title"], "UTF-16", "auto")) . "\r\n".
+    "X-Wii-MB-NoReply: 1\r\n\r\n";
 
-	$raw_description = \Soundasleep\Html2Text::convert($item->get_content(), array("drop_links" => true));
+    $raw_description = \Soundasleep\Html2Text::convert($item->get_content(), array("drop_links" => true));
     $description = mb_convert_encoding($item->get_title(), "UTF-8", "auto") . "\r\n";
-	$description .= mb_convert_encoding($raw_description, "UTF-8", "auto");
+    $description .= mb_convert_encoding($raw_description, "UTF-8", "auto");
 
-	echo $description . "\r\n\r\n" . "-- " . $item->get_link() . "\r\n\r\n";
+    echo $description . "\r\n\r\n" . "-- " . $item->get_link() . "\r\n\r\n";
     
-	/* Create the chjump used for opening the link in the Internet Channel. */
+    /* Create the chjump used for opening the link in the Internet Channel. */
 
-	/*echo "--".$wc24mimebounary."\r\n".
-	"Content-Type: Application/X-Wii-MsgBoard; name=internet_jump.arc\r\n\r\n".
-	"Content-Transfer-Encoding: base64\r\n".
-	"Content-Disposition: attachment; filename=internet_jump.arc\r\n\r\n";
+    /*echo "--".$wc24mimebounary."\r\n".
+    "Content-Type: Application/X-Wii-MsgBoard; name=internet_jump.arc\r\n\r\n".
+    "Content-Transfer-Encoding: base64\r\n".
+    "Content-Disposition: attachment; filename=internet_jump.arc\r\n\r\n";
 
-	$h = pack("H*", "43684A7000000034000000010000000000010001484144410000002000000014") . $item->get_link();
-	mkdir("./tmp");
-	$f = fopen("./tmp/chjump.bin", "wb+");
-	fwrite($f, $h);
-	$create_u8 = shell_exec("wszst CREATE tmp --u8 --no-compress");
-	$u8_output = fopen("tmp.u8", "rb"); # I wish Wiimm set the output extension to be .arc in this case. Oh well..
-	$chjump = base64_encode(fread($u8_output, filesize("tmp.u8")));
-	echo $chjump . "\r\n\r\n";
-	fclose($u8_output);*/
+    $h = pack("H*", "43684A7000000034000000010000000000010001484144410000002000000014") . $item->get_link();
+    mkdir("./tmp");
+    $f = fopen("./tmp/chjump.bin", "wb+");
+    fwrite($f, $h);
+    $create_u8 = shell_exec("wszst CREATE tmp --u8 --no-compress");
+    $u8_output = fopen("tmp.u8", "rb"); # I wish Wiimm set the output extension to be .arc in this case. Oh well..
+    $chjump = base64_encode(fread($u8_output, filesize("tmp.u8")));
+    echo $chjump . "\r\n\r\n";
+    fclose($u8_output);*/
     
     if ($httpModSince == 0) {
         break;  // Show only one entry for first request
